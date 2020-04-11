712d9880aac7f558eecb591e5095bd6a4387e9df
Jacob Hyde <jacobosity@gmail.com>
2020-04-11 23:31:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,9 +2,9 @@ def find_even_values(src)
    row_index = 0
 while row_index < src.count do
   element_index = 0
-  while element_index < spice_rack[row_index].count do
+  while element_index < src[row_index].count do
     if element_index % 2 == 0 
-      puts element_index
+      puts src[element_index]
     element_index += 1
     end
   end
